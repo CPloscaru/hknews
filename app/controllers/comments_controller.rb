0c75c569d716b556da5f8724b30c9704a09a5602
Calin Ploscaru <ytcognito@gmail.com>
2025-04-08 12:02:17 +0200

    show V1 post done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 class CommentsController < ApplicationController
 
-
   def create
     @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = Post.find(params[:post_id])
+    @comment.save
+    redirect_to post_path(@comment.post)
   end
 
-  
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
